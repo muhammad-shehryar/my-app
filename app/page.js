4a1c334fe41047538739066293e277e713916842
shaheryar <muhammadshaheryar50@gmai.com>
2024-07-10 19:20:40 +0500

    fixed

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1 className='flex justify-center items-center h-[100vh] w-full bg-red-500 text-white text-4xl font-extrabold'>Hello</h1>
+      <h1 className='flex justify-center items-center h-[100vh] w-full bg-red-500 text-white text-4xl font-extrabold'>Hellosss</h1>
     </div>
   )
 }
